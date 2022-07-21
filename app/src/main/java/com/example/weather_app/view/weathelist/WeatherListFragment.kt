becf165e4f94b8ba3abb9d4059c17147f14f824e
Patima05 <Patimat777@gmail.com>
2022-07-22 00:45:19 +0300

    Урок 4 | добавлены 2 extention-функции для вывода SnackBar

--- app/src/main/java/com/example/weather_app/view/weathelist/WeatherListFragment.kt
+++ app/src/main/java/com/example/weather_app/view/weathelist/WeatherListFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
 import com.example.weather_app.R
 import com.example.weather_app.databinding.FragmentWeatherListBinding
 import com.example.weather_app.domain.Weather
@@ -15,6 +16,7 @@ import com.example.weather_app.view.details.DetailsFragment
 import com.example.weather_app.view.details.OnItemClick
 import com.example.weather_app.viewmodel.AppState
 import com.example.weather_app.viewmodel.WeatherListViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class WeatherListFragment : Fragment(), OnItemClick {
 
@@ -29,23 +31,25 @@ class WeatherListFragment : Fragment(), OnItemClick {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+        savedInstanceState: Bundle?
+    ): View? {
         binding = FragmentWeatherListBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(WeatherListViewModel :: class.java)
-        viewModel.getLiveData().observe(viewLifecycleOwner
+        viewModel = ViewModelProvider(this).get(WeatherListViewModel::class.java)
+        viewModel.getLiveData().observe(
+            viewLifecycleOwner
         ) { t -> renderData(t) }
 
-        binding.weatherListFragmentFAB.setOnClickListener{
+        binding.weatherListFragmentFAB.setOnClickListener {
             isRussian = !isRussian
-            if(isRussian){
+            if (isRussian) {
                 viewModel.getWeatherListForRussia()
                 binding.weatherListFragmentFAB.setImageResource(R.drawable.ic_earth)
-            }else{
+            } else {
                 viewModel.getWeatherListForWorld()
                 binding.weatherListFragmentFAB.setImageResource(R.drawable.ic_russia)
             }
@@ -56,15 +60,16 @@ class WeatherListFragment : Fragment(), OnItemClick {
     private fun renderData(appState: AppState) {
         when (appState) {
             is AppState.Error -> {
-                Toast.makeText(
-                    requireContext(),
-                    "Ошибка загрузки данных данных",
-                    Toast.LENGTH_SHORT
-                ).show()
+                binding.root.showErrorMessage(R.string.data_error,R.string.try_again) { _ ->
+                    if (isRussian){
+                        viewModel.getWeatherListForWorld()
+                    }else{
+                        viewModel.getWeatherListForRussia()
+                    }
+                }
             }
             is AppState.loading -> {
-                Toast.makeText(requireContext(), "Выполняется загрузка данных", Toast.LENGTH_SHORT)
-                    .show()
+                binding.root.showLoadingMessage(R.string.data_loading)
             }
             is AppState.SuccessOne -> {
                 val result = appState.weatherData
@@ -73,7 +78,6 @@ class WeatherListFragment : Fragment(), OnItemClick {
                 binding.fragmentRecyclerView.adapter =
                     WeatherListAdapter(appState.weatherListData, this)
             }
-
         }
     }
 
@@ -82,6 +86,12 @@ class WeatherListFragment : Fragment(), OnItemClick {
             R.id.container, DetailsFragment.newInstance(weather)
         ).addToBackStack("").commit()
     }
+}
 
+private fun View.showLoadingMessage(stringRef: Int) {
+    Snackbar.make(this, stringRef, Snackbar.LENGTH_SHORT).show()
+}
 
+private fun View.showErrorMessage(stringRef: Int, stringBlockRef: Int, block:(v:View)->Unit) {
+    Snackbar.make(this, stringRef, Snackbar.LENGTH_SHORT).setAction(stringBlockRef, block).show()
 }
